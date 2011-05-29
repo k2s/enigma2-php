da51463d6986a4bc3b905df9381898928e72724b
unknown <k2s@.(none)>
2011-05-29 23:48:38 +0200

    add enigma2 methods (still not clean interface)

--- enigma2.php
+++ enigma2.php
@@ -9,9 +9,17 @@
 class Enigma2Ftp
 {
     protected $_ftpStream;
+    protected $_host;
+    protected $_user;
+    protected $_passwd;
 
     public function __construct($host, $user="root", $passwd="")
     {
+        $this->_host = $host;
+        $this->_user = $user;
+        $this->_passwd = $passwd;
+
+        // connect FTP
         $this->_ftpStream = ftp_connect($host);
 	    // login to FTP server
 	    $login = ftp_login($this->_ftpStream, $user, $passwd);
@@ -48,13 +56,43 @@ class Enigma2Ftp
         return $target;
     }
 
+    /**
+     * @param  $fn
+     * @param  $target
+     * @return bool
+     */
     public function put($fn, $target)
     {
-
+        return ftp_put($this->_ftpStream, $target, $fn, FTP_ASCII);
     }
 
-    public function putBouquets($path)
+    public function putBouquets($folderName)
     {
+        // delete existing files
+        $files = ftp_nlist($this->_ftpStream, '/etc/enigma2/');
+        foreach ($files as $fn) {
+            if (pathinfo($fn, PATHINFO_EXTENSION)=='tv') {
+                ftp_delete($this->_ftpStream, $fn);
+            }
+        }
 
+        // upload new files
+        foreach (glob("$folderName/*.tv") as $filename) {
+            $fn = pathinfo($filename, PATHINFO_BASENAME);
+            ftp_put($this->_ftpStream, '/etc/enigma2/'.$fn, $filename, FTP_ASCII);
+        }
+    }
+
+    public function reload()
+    {
+	file_get_contents("http://".$this->_host."/web/servicelistreload?mode=2");
+        return true;	
+
+        // TODO fallback to enigma2 restart 
+        /*require_once "telnet.php";
+        $telnet = new Telnet($this->_host);
+        $telnet->connect();
+        $telnet->login($this->_user, $this->_passwd);
+        $telnet->exec("killall -9 enigma2");*/
     }
 }
